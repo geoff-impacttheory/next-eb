2104c18d4b521c7f80444ddc614d0d54ab9df1e3
geoff-impacttheory <100635493+geoff-impacttheory@users.noreply.github.com>
2022-04-04 16:20:36 -0700

    Update replace_env.sh

--- scripts/replace_env.sh
+++ scripts/replace_env.sh
@@ -12,9 +12,8 @@ filename="Dockerfile"
 SEARCH_FOR=$1
 REPLACE_WITH=$2
 
-# replace="Happy Birthday! Many Many Happy Returns!"
 REPLACE_WITH_ESCAPED="${REPLACE_WITH////\\/}"
 
 if [[ $REPLACE_WITH_ESCAPED != "" ]]; then
-sed -i '' "s/$SEARCH_FOR/$REPLACE_WITH_ESCAPED/" "Dockerfile"
+sed -i "s/$SEARCH_FOR/$REPLACE_WITH_ESCAPED/" "Dockerfile"
 fi
