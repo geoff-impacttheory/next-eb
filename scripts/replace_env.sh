9e5e7e2d4fc0807dc04c04f037c517d1fdc163f5
geoff-impacttheory <100635493+geoff-impacttheory@users.noreply.github.com>
2022-04-04 16:15:37 -0700

    search and replace variables

--- scripts/replace_env.sh
+++ scripts/replace_env.sh
@@ -9,19 +9,12 @@ filename="Dockerfile"
 # Take the replace string
 # read -p "Enter the replace string: " replace
 
-API_ENDPOINT=$1
-OTHER_VAR=$2
+SEARCH_FOR=$1
+REPLACE_WITH=$2
 
 # replace="Happy Birthday! Many Many Happy Returns!"
-API_ENDPOINT_ESCAPED="${API_ENDPOINT////\\/}"
-OTHER_VAR_ESCAPED="${OTHER_VAR////\\/}"
-# echo $escaped
+REPLACE_WITH_ESCAPED="${REPLACE_WITH////\\/}"
 
-if [[ $API_ENDPOINT_ESCAPED != "" ]]; then
-sed -i "s/api_endpoint/$API_ENDPOINT_ESCAPED/" "Dockerfile"
-#echo "$(<Dockerfile )"
-fi
-
-if [[ $OTHER_VAR_ESCAPED != "" ]]; then
-sed -i "s/secondvar/$OTHER_VAR_ESCAPED/" "Dockerfile"
+if [[ $REPLACE_WITH_ESCAPED != "" ]]; then
+sed -i '' "s/$SEARCH_FOR/$REPLACE_WITH_ESCAPED/" "Dockerfile"
 fi
