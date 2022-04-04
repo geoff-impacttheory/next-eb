46691a7c308bceab7ce4d9a9c1e5a8cb8b0c17d9
geoff-impacttheory <100635493+geoff-impacttheory@users.noreply.github.com>
2022-04-04 12:55:38 -0700

    auto-dockerfile

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to {process.env.NEXT_PUBLIC_API_URL || "Didn't get the env var"}
+        NEXT_PUBLIC_API_URL = {process.env.NEXT_PUBLIC_API_URL || "Didn't get the env var"}
         </h1>
 
         <p className={styles.description}>
@@ -26,16 +26,6 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
       </footer>
     </div>
   )
